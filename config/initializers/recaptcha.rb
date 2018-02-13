3f5effa538f3d3292731d708f3f487905f1afc43
German Ostaszynski <german92@gmail.com>
2018-02-13 11:41:00 -0600

    Recaptcha recognizes development and production keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,11 @@
 Recaptcha.configure do |config|
-  config.site_key   = Rails.application.secrets[:RECAPTCHA_SITE_KEY]
-  config.secret_key = Rails.application.secrets[:RECAPTCHA_PRIVATE_KEY]
+
+  if Rails.env.production?
+  	config.site_key   = Rails.application.secrets[:RECAPTCHA_SITE_KEY]
+  	config.secret_key = Rails.application.secrets[:RECAPTCHA_PRIVATE_KEY]  
+  else
+  	config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  	config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  end
+
 end
