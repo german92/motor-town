b89cee3a638c2b9294a8582499e8590e12d55641
German Ostaszynski <german92@gmail.com>
2018-02-13 10:50:46 -0600

    Tell recaptcha were to pull the keys from

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,9 +19,6 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
-  RECAPTCHA_SITE_KEY = Rails.application.secrets.RECAPTCHA_SITE_KEY
-  RECAPTCHA_PRIVATE_KEY = Rails.application.secrets.RECAPTCHA_PRIVATE_KEY
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
