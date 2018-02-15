19ab1d18a35e37ef696017b4417cd8faf6761de6
German Ostaszynski <german92@gmail.com>
2018-02-15 17:49:18 -0600

    Modify mailer production smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,9 +64,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV["SMTP_HOST"],
-    port:                 ENV["SMTP_PORT"],
-    domain:               ENV["SMTP_DOMAIN"],
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'motortowncr.com',
     user_name:            ENV["SMTP_USERNAME"],
     password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
